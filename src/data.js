166368dc5b37bed0519aa2fbc5ad2a698348de53
rafael.vasconcelos <rafaelvasconcelos13@gmail.com>
2022-06-08 15:41:54 -0300

    quotes added, other small changes, some unnecessary files deleted

--- src/data.js
+++ src/data.js
@@ -1,81 +1,112 @@
-const data = [{
+const data = [
+  {
     quote: `Existem mais coisas suscetíveis 
     de nos assustar do que existem de nos derrotar; sofremos 
     mais na imaginação do que na realidade.`,
-    author: `Sêneca`    
-},
-{
+    author: `Sêneca`,
+  },
+  {
     quote: `Sábio é aquele que conhece os limites da própria ignorância.`,
-    author: `Sócrates`    
-},
-{
+    author: `Sócrates`,
+  },
+  {
     quote: `Às vezes ouço passar o vento; e só de ouvir o vento passar, vale a pena ter nascido.`,
-    author: `Fernando Pessoa`    
-},
-{
+    author: `Fernando Pessoa`,
+  },
+  {
     quote: `Tudo vale a pena quando a alma não é pequena.`,
-    author: `Fernando Pessoa`    
-},
-{
+    author: `Fernando Pessoa`,
+  },
+  {
     quote: `É mais fácil obter o que se deseja com um sorriso do que à ponta da espada.`,
-    author: `William Shakespeare`    
-},
-{
+    author: `William Shakespeare`,
+  },
+  {
     quote: `Os miseráveis não têm outro remédio a não ser a esperança.`,
-    author: `William Shakespeare`    
-},
-{
+    author: `William Shakespeare`,
+  },
+  {
     quote: `Um dia é preciso parar de sonhar e, de algum modo, partir.`,
-    author: `Amyr Klink`    
-},
-{
+    author: `Amyr Klink`,
+  },
+  {
     quote: `Prefiro os que me criticam, porque me corrigem, aos que me elogiam, porque me corrompem.`,
-    author: `Santo Agostinho`    
-},
-{
+    author: `Santo Agostinho`,
+  },
+  {
     quote: `Para te contentares olha para os que possuem menos do que tu, e não para os que possuem mais.`,
-    author: `Benjamin Franklin`    
-},
-{
+    author: `Benjamin Franklin`,
+  },
+  {
     quote: `Nada de desgosto, nem de desânimo; se acabas de fracassar, recomeça.`,
-    author: `Marco Aurélio`    
-},
-{
+    author: `Marco Aurélio`,
+  },
+  {
     quote: `A vida é a arte do encontro, embora haja tanto desencontro pela vida.`,
-    author: `Vinicius de Moraes`    
-},
-{
+    author: `Vinicius de Moraes`,
+  },
+  {
     quote: `A morte não é nada para nós, pois, quando existimos, não existe a morte, e quando existe a morte, não existimos mais.`,
-    author: `Epicuro`    
-},
-{
+    author: `Epicuro`,
+  },
+  {
     quote: `É impossível para um homem aprender aquilo que ele acha que já sabe.`,
-    author: `Epicteto`    
-},
-{
+    author: `Epicteto`,
+  },
+  {
     quote: `Nada de grande se cria de repente.`,
-    author: `Epicteto`    
-},
-{
+    author: `Epicteto`,
+  },
+  {
     quote: `Um homem deve sempre adaptar-se às circunstâncias que o cercam e dirigir seus esforços a um novo objetivo, quando vê o anterior ser destruído.`,
-    author: `Ernest Shackleton`    
-},
-// {
-//     quote: `A verdadeira medida de um homem não se vê na forma como se comporta em momentos de conforto e conveniência, mas em como se mantém em tempos de controvérsia e desafio.`,
-//     author: `Martin Luther King`    
-// },
-{
+    author: `Ernest Shackleton`,
+  },
+  {
     quote: `O que me preocupa não é o grito dos maus. É o silêncio dos bons.`,
-    author: `Martin Luther King`    
-},
-{
+    author: `Martin Luther King`,
+  },
+  {
     quote: `Cansei-me de ser moderno. Quero ser eterno.`,
-    author: `Pablo Picasso`
-},
-{
+    author: `Pablo Picasso`,
+  },
+  {
     quote: `Quando se navega sem destino, nenhum vento é favorável.`,
-    author: `Sêneca`
-}
-]
+    author: `Sêneca`,
+  },
+  {
+    quote: `É preciso dizer a verdade apenas a quem está disposto a ouvi-la.`,
+    author: `Sêneca`,
+  },
+  {
+    quote: `Apressa-te a viver bem e pensa que cada dia é, por si só, uma vida.`,
+    author: `Sêneca`,
+  },
+  {
+    quote: `Qualquer pessoa capaz de te irritar se torna teu mestre;
+     ela consegue te irritar somente quando você se permite ser perturbado por ela.`,
+    author: `Epicteto`,
+  },
+  {
+    quote: `Ter muitos amigos é não ter nenhum.`,
+    author: `Aristóteles`,
+  },
+  {
+    quote: `O sábio nunca diz tudo o que pensa, mas sempre pensa tudo o que diz.`,
+    author: `Aristóteles`,
+  },
+  {
+    quote: `Ser feliz sem motivo é a mais autêntica forma de felicidade.`,
+    author: `Carlos Drummond de Andrade`,
+  },
+  {
+    quote: `Se não está em suas mãos mudar uma situação que te causa dor,
+     sempre poderá escolher a atitude com que encara esse sofrimento.`,
+    author: `Viktor Frankl`,
+  },
+  {
+    quote: `O homem é o filho do seu passado, mas não o seu escravo, e é o pai do seu futuro.`,
+    author: `Viktor Frankl`,
+  },
+];
 
-export default data
+export default data;
